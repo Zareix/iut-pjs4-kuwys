11a4f868ce62c342d81fe713c8084ed8d3f3b3ce
Zareix <raphcatarino@gmail.com>
2021-01-22 12:54:38 +0100

    fj

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
