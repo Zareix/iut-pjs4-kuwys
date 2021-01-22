139b131366ab2773725792b34e2cc7e8b55f6d5f
Zareix <raphcatarino@gmail.com>
2021-01-22 10:32:24 +0100

    test CSS in JS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const styles = {
+  backgroundColor : "white",
+  color : "black"
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p class="text-yellow-300">
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code style={styles}>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
