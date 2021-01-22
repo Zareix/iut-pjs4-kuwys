75b6d63dfb0146475fb15552aa5d5101cddafc05
Zareix <raphcatarino@gmail.com>
2021-01-22 10:00:42 +0100

    installed tailwind CSS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p class="text-yellow-300">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
