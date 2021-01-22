58722a5c2b6a9998c8fc509d3c56041b46c7b409
Zareix <raphcatarino@gmail.com>
2021-01-22 11:13:12 +0100

    Update Test.js

--- src/components/Test.js
+++ src/components/Test.js
@@ -17,6 +17,8 @@ const Test = (props) => {
 
   // Equivalent de componentDidMount, etc. d'une classe React
   // Se lance au rendu du composant et lance le return quand le composant disparait
+  // [x, y, z] la liste des variables dont useEffect dépend
+  // ATTENTION AUX BOUCLES : si useEffect appelle un setter du state !
   useEffect(() => {
     console.log("rendu de test : " + props.num)
     return () => {
