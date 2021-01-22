d4116f227fa6e9e6fc09ca4a83071664ff771140
Zareix <raphcatarino@gmail.com>
2021-01-22 11:07:31 +0100

    ptits tests pour vous la mif

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import Test from "./components/Test"
 
 const styles = {
-  backgroundColor : "white",
-  color : "black"
+  backgroundColor: "white",
+  color: "black",
 }
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="font-bold">KUWYS</h1>
+        <h2>Keeping Up With Your Studies</h2>
         <p class="text-yellow-300">
           Edit <code style={styles}>src/App.js</code> and save to reload.
         </p>
@@ -22,9 +25,12 @@ function App() {
         >
           Learn React
         </a>
+        <h4 className="mt-10">Petits tests pour vous la mif :</h4>
+        <Test num={1}></Test>
+        <Test num={2}></Test>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
