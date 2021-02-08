950f73cf7a147829fd7e26f032835399d7259d41
Zareix <raphcatarino@gmail.com>
2021-02-08 20:07:17 +0100

    minor fixes

--- src/components/PresentationButton.js
+++ src/components/PresentationButton.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react"
 
 const PresentationButton = (props) => {
-    return (
-        <div href="#" className="w-full flex items-center justify-center presentationButton grow px-8 py-3 font-bold rounded-lg text-black md:py-4 md:text-xs md:px-10">
-            <a>{props.text}</a>
-        </div>
-    )
+  return (
+    <a
+      href="#"
+      className="w-full flex items-center justify-center presentationButton grow px-8 py-3 font-bold rounded-lg text-black md:py-4 md:text-xs md:px-10"
+    >
+      <span>{props.text}</span>
+    </a>
+  )
 }
 
 export default PresentationButton
