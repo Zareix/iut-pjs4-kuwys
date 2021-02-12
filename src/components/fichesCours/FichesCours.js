96f67058e31de64b47c961741c1b5beb3cd38b4d
Mohamed <mohamed-siso@hotmail.fr>
2021-02-12 12:45:32 +0100

    début page Fiches et cours

--- src/components/fichesCours/FichesCours.js
+++ src/components/fichesCours/FichesCours.js
@@ -1,25 +1,77 @@
 import React, { useState, useEffect, useContext } from "react"
 
-import { DbContext } from "../../App"
-
 import Fiche from "./Fiche"
-import Gui from "../gui/Gui"
-
+import { DbContext } from "../../App"
+import Pdf from "../Pdf"
 const FichesCours = () => {
   const [selectedFiche, setSelectedFiche] = useState()
+  const firebase = useContext(DbContext)
+  const [snapshotFiche, setSnapshotFiche] = useState([])
+  const [snapshotCours, setSnapshotCours] = useState([])
 
   const firebase = useContext(DbContext)
 
   useEffect(() => {
+    const getSnapshot = async () => {
+      const listeFicheRef = firebase.firestore().collection("posts")
+      const dataFiche = await listeFicheRef
+        .where("typeDoc", "==", "fiche")
+        .get()
+      const dataCours = await listeFicheRef
+        .where("typeDoc", "==", "cours")
+        .get()
+      dataFiche.forEach((item) => {
+        setSnapshotFiche([...snapshotFiche, item.data()])
+      })
+
+      dataCours.forEach((item) => {
+        setSnapshotCours([...snapshotCours, item.data()])
+      })
+    }
+    getSnapshot()
     return () => {}
   }, [])
 
-  return (
-		<Gui>
-			{selectedFiche ? <Fiche /> : <div></div>}
-			<Fiche></Fiche>
-		</Gui>
-	)
+  return selectedFiche ? (
+    <Fiche fiche={selectedFiche} />
+  ) : (
+    <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-4">
+      {snapshotFiche &&
+        snapshotFiche.map((f, index) => (
+          <div className="fiches" key={index}>
+            <figure className="bg-gray-100 rounded-xl p-8">
+            <Pdf pdfPath={f.documents[0]}></Pdf>
+
+              <div className="pt-6 space-y-4">
+                <blockquote>
+                  <p className="text-lg font-semibold">{f.titre}</p>
+                </blockquote>
+                <figcaption className="font-medium">
+                  <div className="text-cyan-600"></div>
+                  <div></div>
+                </figcaption>
+              </div>
+            </figure>
+          </div>
+        ))}
+      {snapshotCours &&
+        snapshotCours.map((f, index) => (
+          <div className="cours" key={index}>
+            <figure className="bg-gray-100 rounded-xl p-8">
+              <div className="pt-6 space-y-4">
+                <blockquote>
+                  <p className="text-lg font-semibold">{f.titre}</p>
+                </blockquote>
+                <figcaption className="font-medium">
+                  <div className="text-cyan-600"></div>
+                  <div></div>
+                </figcaption>
+              </div>
+            </figure>
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default FichesCours
