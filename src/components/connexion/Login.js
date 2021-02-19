2c0eec789ff3281236ad9fdbd374988f07d673c1
Arthur <pigeon.arthur@hotmail.fr>
2021-02-19 12:36:58 +0100

    api login

--- src/components/connexion/Login.js
+++ src/components/connexion/Login.js
@@ -1,54 +1,100 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useState } from "react";
+import axios from "axios";
 
-import { Link } from "react-router-dom"
+import { Redirect } from "react-router-dom";
 
-import { DbContext } from "../../App"
-
-var firebaseui = require("firebaseui")
+import { DbContext } from "../../App";
+import { tokenToString } from "typescript";
 
 const Login = (props) => {
-  const { firebase, auth } = useContext(DbContext)
+  const { setToken } = useContext(DbContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const [redirect, setRedirect] = useState(false);
 
-  useEffect(() => {
-    const ui = new firebaseui.auth.AuthUI(auth)
-
-    var uiConfig = {
-      callbacks: {
-        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-          console.log(authResult.user.uid)
-          document.getElementById("backButton").style.display = "block"
-          return false
-        },
-        uiShown: function () {
-          document.getElementById("loader").style.display = "none"
-        },
-      },
-      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-      signInFlow: "popup",
-      signInSuccessUrl: "#",
-      signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
-      // Terms of service url.
-      tosUrl: "<your-tos-url>",
-      // Privacy policy url.
-      privacyPolicyUrl: "<your-privacy-policy-url>",
-    }
-
-    ui.start("#firebaseui-auth-container", uiConfig)
-    return () => {
-      ui.delete()
-    }
-  }, [auth, firebase])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({});
+    axios
+      .post("/login", { email, password })
+      .then((res) => {
+        console.log(res.data);
+        setToken(res.data.token);
+        setRedirect(true);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setErrors({ ...err.response.data });
+      });
+  };
+  if (redirect) return <Redirect to="/" />;
 
   return (
-    <div>
-      <h1>Welcome to My Awesome App</h1>
-      <div id="firebaseui-auth-container"></div>
-      <div id="loader">Loading...</div>
-      <Link id="backButton" to="home" style={{ display: "none" }}>
-        Back
-      </Link>
+    <div className="w-full max-w-xs">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="text"
+            placeholder="user@email.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-xs italic">{errors.email}</p>
+          )}
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-xs italic">{errors.password}</p>
+          )}
+          {errors.general && (
+            <p className="text-red-500 text-xs italic">{errors.general}</p>
+          )}
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2020 Acme Corp. All rights reserved.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
