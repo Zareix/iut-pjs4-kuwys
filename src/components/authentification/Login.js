0a4d9f4339b5d5e2ca060ff7538f68f04392322a
Zareix <raphcatarino@gmail.com>
2021-03-05 12:49:24 +0100

    cleanup

--- src/components/authentification/Login.js
+++ src/components/authentification/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import { Redirect, useLocation } from 'react-router-dom'
 
 import API from '../../util/api'
