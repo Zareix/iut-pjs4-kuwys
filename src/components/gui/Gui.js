3a647202b4f1805d85e47eb9b2446c216c62493d
Zareix <raphcatarino@gmail.com>
2021-03-24 12:13:16 +0100

    simplified gui

--- src/components/gui/Gui.js
+++ src/components/gui/Gui.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import {useGlobalContext} from '../../util/context'
+import { useGlobalContext } from '../../util/context'
 import TopBar from './TopBar'
 import Footer from './Footer'
 
@@ -12,25 +12,24 @@ const Gui = (props) => {
       <TopBar />
       <div style={{ minHeight: '100vh' }} className="grid grid-cols-6 z-50">
         <div
-          className="col-span-1 w-1/6 fixed top-12 h-full p-10"
+          className="col-span-1 fixed top-12 h-full pt-6 p-12"
           style={{ backgroundColor: '#ECECEC' }}
         >
           <div className="grid justify-items-center my-10 text-sm">
-            <img src={user.imageUrl} className="w-16 mb-2" alt="profil pp"/>
+            <img src={user.imageUrl} className="w-16 mb-2" alt="profil pp" />
             <p>{user.username}</p>
-            {/* TODO A modif avec nom prenom mais pas dispo pour le moment */}
           </div>
           <div className="grid grid-cols-2">{/* icons */}</div>
-          <div className="grid gap-4 ml-4 text-xs">
+          <div className="grid gap-4 justify-center text-xs">
             <Link to="/">Accueil</Link>
             <Link to="/profil">Profil</Link>
             <p>Espace Ecole</p>
             <Link to="/fichescours">Fiches & Cours</Link>
             <p>Forum Q&R</p>
-            <p>Groupes de travail</p>
+            <Link to="/groupestravail">Groupes de travail</Link>
           </div>
         </div>
-        <div className="col-start-2 col-span-6 mt-12">{props.children}</div>
+        <div className="col-start-2 col-span-6 mt-12 p-6 min-h-screen">{props.children}</div>
         <div className="col-start-2 col-span-6">
           <Footer />
         </div>
--- src/components/gui/TopBar.js
+++ src/components/gui/TopBar.js
@@ -6,11 +6,10 @@ const TopBar = () => {
   const { logout } = useGlobalContext()
   return (
     <div
-      style={{ backgroundColor: '#F7B91C' }}
-      className="h-12 p-3 text-white grid grid-cols-2 fixed w-full z-50"
+      className="h-12 p-3 text-white grid grid-cols-2 fixed w-full ourYellowBg z-50"
     >
       <Link to="/">[KUWYS LOGO]</Link>
-      <div className="text-right" onClick={logout}>Déconnexion</div>
+      <div className="text-right cursor-pointer" onClick={logout}>Déconnexion</div>
     </div>
   )
 }
