30e585a231790b080f0c86d21c256695d5ea9b8b
Zareix <raphcatarino@gmail.com>
2021-03-27 23:21:38 +0100

    Update NavigationPanel.js

--- src/components/gui/NavigationPanel.js
+++ src/components/gui/NavigationPanel.js
@@ -5,14 +5,12 @@ import { useGlobalContext } from '../../util/context'
 
 const NavigationPanel = (props) => {
   const { user } = useGlobalContext()
-  const {active} = props
 
   return (
     <div className="grid justify-items-center  text-base md:text-sm">
       <Link to="/profil" className="grid justify-items-center my-10">
         <img src={user.imageUrl} className="w-16 mb-2" alt="profil pp" />
         <p>{user.username}</p>
-        {/* TODO A modif avec nom prenom mais pas dispo pour le moment */}
       </Link>
       <div className="grid grid-cols-2">{/* icons */}</div>
       <div className="grid gap-4 w-2/3 md:w-full">
