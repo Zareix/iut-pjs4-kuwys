0afa8c7d97fadf1b4d809a90d34f16437a4d027c
Zareix <raphcatarino@gmail.com>
2021-03-30 16:07:17 +0200

    animation

--- src/components/accueilUser/AccueilFiches.js
+++ src/components/accueilUser/AccueilFiches.js
@@ -1,27 +1,30 @@
 import React from 'react'
 
-import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade'
+import Zoom from 'react-reveal/Zoom'
 
 import AllPost from '../fichesCours/AllPost'
 
 const FavFiches = (props) => {
   const { posts, retour, title, type } = props
 
-
-
   return (
-    <Zoom className="transition-opacity">
-      <button
-        className="border bg-yellow-300 text-white font-bold text-xs h-6 mt-4 px-2 rounded-full"
-        onClick={retour}
-      >
-        Retour
-      </button>
-      <h1 className="text-xl font-semibold my-5">{title}</h1>
-      <div className="border-2 border-yellow-500 border-opacity-50 shadow-lg p-5">
-        <AllPost posts={posts} type={type} />
+    <Fade className="transition-opacity">
+      <div>
+        <button
+          className="border bg-yellow-300 text-white font-bold text-xs h-6 mt-4 px-2 rounded-full"
+          onClick={retour}
+        >
+          Retour
+        </button>
+        <h1 className="text-xl font-semibold my-5">{title}</h1>
+        <Zoom>
+          <div className="border-2 border-yellow-500 border-opacity-50 shadow-lg p-5">
+            <AllPost posts={posts} type={type} />
+          </div>
+        </Zoom>
       </div>
-    </Zoom>
+    </Fade>
   )
 }
 
