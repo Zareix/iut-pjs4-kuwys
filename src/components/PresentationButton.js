4936369da12753ea24e1e4a759773decedd7ff29
lolammrn <60889697+lolammrn@users.noreply.github.com>
2021-03-30 12:14:49 +0200

    lola trucs

--- src/components/PresentationButton.js
+++ src/components/PresentationButton.js
@@ -4,7 +4,7 @@ const PresentationButton = (props) => {
   return (
     <a
       href="#"
-      className="w-full flex items-center justify-center presentationButton grow px-8 py-3 font-bold rounded-lg text-black md:py-4 md:text-xs md:px-10"
+      className="w-full flex items-center justify-center bg-white presentationButton grow px-8 py-3 font-bold rounded-lg text-black md:py-4 md:text-xs md:px-10"
     >
       <span>{props.text}</span>
     </a>
